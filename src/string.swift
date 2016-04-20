1d660a16ce6e6c1949476075459bce338f6aaa49
Johannes Schriewer <hallo@dunkelstern.de>
2016-04-20 02:28:28 +0200

    Implement some string extensions

--- src/string.swift
+++ src/string.swift
@@ -1,8 +1,38 @@
-// # TODO List:
-//
-// ## String extension:
-//
-// - init(path:) (string from path instance)
-// - init?(loadFromPath:) throws (load string from file)
-// - write(toFile:atomically:)
 
+public extension String {
+
+    /// Create string from path
+    ///
+    /// - Parameter path: a path
+    public init(path: Path) {
+        self.init(path.description)
+    }
+
+    /// Load a string from a file
+    ///
+    /// - Parameter loadFromFile: the filename
+    public init?(loadFromFile path: Path) throws {
+        let f = try File(path: path, mode: .ReadOnly)
+        if let s: String = try f.readAll() {
+            self.init(s)
+        } else {
+            return nil
+        }
+    }
+
+    /// Write a string to a file
+    ///
+    /// - Parameter to: file to write to
+    public func write(to file: File) throws {
+        try file.write(string: self)
+    }
+
+    /// Write a string to a new file
+    ///
+    /// - Parameter to: filename to write the string to
+    public func write(to path: Path) throws {
+        let f = try File(path: path, mode: .WriteOnly)
+        try f.write(string: self)
+        f.flush()
+    }
+}
