39850a7d598ace35adca4dafe4c2c64e829f678f
Johannes Schriewer <hallo@dunkelstern.de>
2016-04-24 16:44:26 +0200

    Remove newlines from whitespace character set to mirror NSCharacterSet behaviour

--- src/charset.swift
+++ src/charset.swift
@@ -16,6 +16,15 @@ public class Charset {
 
     public class func isWhitespace(character: Character) -> Bool {
         switch character {
+            case " ", "\t": // ASCII
+                return true
+            default:
+                return false
+        }
+    }
+
+    public class func isWhitespaceOrNewline(character: Character) -> Bool {
+        switch character {
             case " ", "\n", "\r\n", "\t": // ASCII
                 return true
             default:
@@ -24,6 +33,17 @@ public class Charset {
     }
 
     public class func isUnicodeWhitespace(character: Character) -> Bool {
+        switch character {
+            case " ", "\t": // ASCII
+                return true
+            case "\u{00a0}", "\u{1680}", "\u{2000}"..."\u{200a}", "\u{202f}", "\u{205f}", "\u{3000}": // various spaces
+                return true
+            default:
+                return false
+        }
+    }
+
+    public class func isUnicodeWhitespaceOrNewline(character: Character) -> Bool {
         switch character {
             case " ", "\n", "\r\n", "\t": // ASCII
                 return true
