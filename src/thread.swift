71a8e525a253dce2adc50888e1a5c4c15fce92c4
Johannes Schriewer <hallo@dunkelstern.de>
2016-05-30 02:16:30 +0200

    Add documentation to Thread

--- src/thread.swift
+++ src/thread.swift
@@ -18,22 +18,22 @@
     import Darwin
 #endif
 
+/// Thread primitive
 final public class Thread {
     private static let mainThreadID = pthread_self()
 
-    private let threadMain: (Any? -> Any?)
+    private let threadMain: (Void -> Any?)
     private let detached: Bool
     private var threadID: pthread_t? = nil
 
-    private var argument: Any?
-    private var result: Any?
-
     typealias ThreadMainFunction = @convention(c) (UnsafeMutablePointer<Void>!) -> UnsafeMutablePointer<Void>!
 
-    public init(argument: Any? = nil, detached: Bool = false, _ block: (Any? -> Any?)) {
+    /// Initialize and run a thread
+    ///
+    /// - parameter block: block to run in a new thread, the return value of the block can be aquired with `wait()`
+    public init(_ block: (Void -> Any?)) {
         self.threadMain = block
-        self.argument = argument
-        self.detached = detached
+        self.detached = false
 
         var attr = pthread_attr_t()
         pthread_attr_init(&attr)
@@ -42,18 +42,20 @@ final public class Thread {
 
         pthread_create(&self.threadID, &attr, { arg in
             let thread = unsafeBitCast(arg, to: Thread.self)
-            let result = thread.threadMain(thread.argument)
+            let result = thread.threadMain()
             return unsafeBitCast(result, to: UnsafeMutablePointer<Void>!.self)
         }, nil)
     }
 
-    public init(argument: Any? = nil, detached: Bool = false, _ block: (Void -> Void)) {
-        self.threadMain = { arg in
+    /// Initialize and run a detached thread that does not return a value
+    ///
+    /// - parameter block: block to detach to new thread
+    public init(_ block: (Void -> Void)) {
+        self.threadMain = {
             block()
             return nil
         }
-        self.argument = argument
-        self.detached = detached
+        self.detached = true
 
         var attr = pthread_attr_t()
         pthread_attr_init(&attr)
@@ -62,11 +64,14 @@ final public class Thread {
 
         pthread_create(&self.threadID, &attr, { arg in
             let thread = unsafeBitCast(arg, to: Thread.self)
-            thread.threadMain(nil)
+            thread.threadMain()
             return nil
         }, unsafeBitCast(self, to: UnsafeMutablePointer<Void>.self))
     }
 
+    /// Wait for the result of a non-detached thread
+    ///
+    /// - returns: Value that the thread closure returned
     public func wait() -> Any? {
         if self.detached {
             var returnValue = UnsafeMutablePointer<Void>(nil)
@@ -77,6 +82,7 @@ final public class Thread {
         return nil
     }
 
+    /// Fetch current thread ID, returns zero for main thread
     public static var threadID: UInt64 {
         let thread = pthread_self()
         if thread == Thread.mainThreadID {
@@ -86,10 +92,12 @@ final public class Thread {
     }
 }
 
+/// Barrier primitive
 final public class Barrier {
     private let mutex = Mutex()
     internal var conditionID = pthread_cond_t()
 
+    /// Initialize a new barrier
     public init() {
         pthread_cond_init(&self.conditionID, nil)
     }
@@ -98,6 +106,9 @@ final public class Barrier {
         pthread_cond_destroy(&self.conditionID)
     }
 
+    /// Wait for a signal (stop at the barrier)
+    ///
+    /// - parameter timeout: optional, time in nano seconds to wait, set to nil or skip to wait forever
     public func wait(timeout: Int? = nil) {
         if let timeout = timeout {
             var ts = timespec()
@@ -111,14 +122,20 @@ final public class Barrier {
         }
     }
 
+    /// Signal the barrier, all threads blocked on that barrier will resume execution
     public func signal() {
         pthread_cond_signal(&self.conditionID)
     }
 }
 
+/// Mutex lock primitive
 final public class Mutex {
     internal var mutexID = pthread_mutex_t()
 
+    /// Initialize new lock
+    ///
+    /// - parameter recursive: Set to true if you want a recursive lock
+    ///                        (same thread may recursively lock multiple times)
     public init(recursive: Bool = false) {
         var attr = pthread_mutexattr_t()
         pthread_mutexattr_init(&attr)
@@ -132,6 +149,11 @@ final public class Mutex {
         pthread_mutex_destroy(&self.mutexID)
     }
 
+    /// Lock the mutex, may throw an error on resource exhaustion or
+    /// multiple locking from the same thread when not recursive
+    /// Usually you could use `try!` to execute this as every error
+    /// this throws is either system resource exhaustion or a programming
+    /// error.
     public func lock() throws {
         let result = pthread_mutex_lock(&self.mutexID)
         if result != 0 {
@@ -139,6 +161,10 @@ final public class Mutex {
         }
     }
 
+    /// Unlock a locked mutex, may throw errors if the mutex was not locked
+    /// Usually you could use `try!` to execute this as every error
+    /// this throws is either system resource exhaustion or a programming
+    /// error.
     public func unlock() throws {
         let result = pthread_mutex_unlock(&self.mutexID)
         if result != 0 {
@@ -146,6 +172,11 @@ final public class Mutex {
         }
     }
 
+    /// Try locking the mutex, returns false if the lock is already held
+    /// somewhere. If it returns true the locking succeeded.
+    /// Usually you could use `try!` to execute this as every error
+    /// this throws is either system resource exhaustion or a programming
+    /// error.
     public func tryLock() throws -> Bool {
         let result = pthread_mutex_trylock(&self.mutexID)
         if result != 0 {
@@ -158,6 +189,9 @@ final public class Mutex {
         return true
     }
 
+    /// Run a block with the lock held, this is the prefered method to use
+    ///
+    /// - parameter cb: block to execute
     public func whileLocking(_ cb: Void -> Void) throws {
         try self.lock()
         cb()
@@ -165,10 +199,17 @@ final public class Mutex {
     }
 }
 
+/// Semaphore primitive
 final public class Semaphore {
-    public let name: String
     private var semaphoreID: UnsafeMutablePointer<sem_t>?
 
+    /// Name of the semaphore
+    public let name: String
+
+    /// Initialize a semaphore
+    ///
+    /// - parameter name: The name
+    /// - parameter count: Resource allocation limit
     public init(name: String, count: UInt32) throws {
         self.name = name
         self.semaphoreID = sem_open(name, O_CREAT)
@@ -181,6 +222,12 @@ final public class Semaphore {
         sem_unlink(name)
     }
 
+    /// Wait for a semaphore. This decrements the resource limit,
+    /// if the limit reaches zero the thread will wait for a `post()`
+    /// from another thread.
+    /// Usually you could use `try!` to execute this as every error
+    /// this throws is either system resource exhaustion or a programming
+    /// error.
     public func wait() throws {
         let result = sem_wait(self.semaphoreID)
         if result != 0 {
@@ -188,6 +235,14 @@ final public class Semaphore {
         }
     }
 
+    /// Test if resources are available
+    ///
+    /// Usually you could use `try!` to execute this as every error
+    /// this throws is either system resource exhaustion or a programming
+    /// error.
+    ///
+    /// - returns: false if the semaphore resources are at zero already,
+    ///            true if the semaphore could be aquired
     public func tryWait() throws -> Bool {
         let result = sem_trywait(self.semaphoreID)
         if result != 0 {
@@ -200,10 +255,15 @@ final public class Semaphore {
         return true
     }
 
+    /// Release a resource to the semaphore
     public func post() {
         sem_post(self.semaphoreID)
     }
 
+    /// Execute a block aquiring resources from the semaphore and return the
+    /// resources afterwards, this is the prefered function to call.
+    ///
+    /// - parameter cb: block to execute
     public func whileLocking(cb: Void -> Void) throws {
         try self.wait()
         cb()
