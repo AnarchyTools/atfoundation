a8207b4e1fa2b94a610a9a9502f1430c3f64165d
Johannes Schriewer <hallo@dunkelstern.de>
2016-06-19 17:07:42 +0200

    Add `hexString` extension to UInt8 and UInt16

--- src/string.swift
+++ src/string.swift
@@ -49,3 +49,61 @@ public extension String {
         f.flush()
     }
 }
+
+// TODO: Make Tests
+
+/// Extension to UInt16 to convert to hex String
+public extension UInt16 {
+
+    /// Convert to hex string
+    ///
+    /// - Parameter padded: set to `true` to pad with zeroes, defaults to `true`
+    /// - Returns: String of maximum length of 4 characters, hex encoded value
+    func hexString(padded: Bool = true) -> String {
+        let dict:[Character] = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+        var result = ""
+        var somethingWritten = false
+        for i in 0...3 {
+            let value = Int(self >> UInt16(((3 - i) * 4)) & 0xf)
+            if !padded && !somethingWritten && value == 0 {
+                continue
+            }
+            somethingWritten = true
+            result.append(dict[value])
+        }
+        if (result.characters.count == 0) {
+            return "0"
+        }
+        return result
+    }
+}
+
+// TODO: Make Tests
+
+/// Extension to UInt8 to convert to hex String
+public extension UInt8 {
+
+    /// Convert to hex string
+    ///
+    /// - Parameter padded: set to `true` to pad with zeroes, defaults to `true`
+    /// - Returns: String of maximum length of 2 characters, hex encoded value
+    func hexString(padded: Bool = true) -> String {
+        let dict:[Character] = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+        var result = ""
+
+        let c1 = Int(self >> 4)
+        let c2 = Int(self & 0xf)
+
+        if c1 == 0 && padded {
+            result.append(dict[c1])
+        } else if c1 > 0 {
+            result.append(dict[c1])
+        }
+        result.append(dict[c2])
+
+        if (result.characters.count == 0) {
+            return "0"
+        }
+        return result
+    }
+}
