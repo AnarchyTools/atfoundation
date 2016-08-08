f139e3f7628aa363000eee98d3fdda915287454b
Drew Crawford <drew@sealedabstract.com>
2016-08-08 16:27:39 -0500

    Update to Swift Preview 4

--- src/syserror.swift
+++ src/syserror.swift
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-public enum SysError: ErrorProtocol {
+public enum SysError: Error {
     // Generic
     case OperationNotPermitted
     case NoSuchEntity(Path?)
